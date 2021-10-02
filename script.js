ac567ce3272ec1c7ed9f1b54e9d385ecb45416b3
mcelhinneyk <mr.mcelhinney@gmail.com>
2021-10-01 19:09:36 -0700

    changed backgroud color of active nav link

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
-const projectImage1 = document.getElementById("project1")
-const projectImage2 = document.getElementById("project2")
-const projectImage3 = document.getElementById("project3")
+const projectImage1 = document.getElementById("project1");
+const projectImage2 = document.getElementById("project2");
+const projectImage3 = document.getElementById("project3");
+const navProject = document.getElementById("projectLink");
+const navAbout = document.getElementById("aboutLink");
+const navContact = document.getElementById("contactLink");
+let currentNavActive = document.getElementsByClassName("active")[0].id;
 
 function replaceImage(item){
     const original = item.getAttribute("src");
-    item.src = "images/commingSoon.png"
+    item.src = "images/commingSoon.png";
     
 }
 
@@ -17,11 +21,11 @@ projectImage1.addEventListener('mouseover',function(){
 })
 
 projectImage2.addEventListener("click", function(){
-    projectImage2.src = "images/commingSoon.png"
+    projectImage2.src = "images/commingSoon.png";
 })
 
 projectImage2.addEventListener("mouseout",function(){
-    projectImage2.src = "images/mitosis.png"
+    projectImage2.src = "images/mitosis.png";
 })
 
 projectImage3.addEventListener("click", function(){
@@ -29,5 +33,24 @@ projectImage3.addEventListener("click", function(){
 })
 
 projectImage3.addEventListener("mouseout",function(){
-    projectImage3.src = "images/BugTracker.jpg"
+    projectImage3.src = "images/BugTracker.jpg";
+})
+
+navProject.addEventListener('click',function(){
+    navContact.className = '';
+    navAbout.className = '';
+    navProject.className = 'active';
+
+})
+
+navAbout.addEventListener('click',function(){
+    navContact.className = '';
+    navProject.className = '';
+    navAbout.className = 'active';
+})
+
+navContact.addEventListener('click',function(){
+    navProject.className = '';
+    navAbout.className = '';
+    navContact.className = 'active';
 })
